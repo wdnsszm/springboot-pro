27639b898f2728bd44c7190b8ee7e196ea3f5b0a
wdnsszm <15610416128@163.com>
2018-12-18 19:20:03 +0800

    初始化项目

---
+++ src/main/java/com/wdnsszm/springboot_pro/SpringbootProApplication.java
@@ -0,0 +1,14 @@
+package com.wdnsszm.springboot_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/wdnsszm/springboot_pro/SpringbootProApplicationTests.java
@@ -0,0 +1,17 @@
+package com.wdnsszm.springboot_pro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootProApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
